ea00405c157c840e63c943a7089b06937f3c9ee6
Yefka <yefkaa@gmail.com>
2015-02-20 22:39:27 +0100

    init

---
+++ ImapAttachementExtractor/ImapExporter.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace ImapAttachementExtractor
+{
+    partial class ImapExporter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImapExporter));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnTestConnection = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtExportFolder = new System.Windows.Forms.TextBox();
+            this.btnExportFolder = new System.Windows.Forms.Button();
+            this.nudPort = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).BeginInit();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adresse e-mail :";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(182, 43);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(170, 22);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mot de passe:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(182, 71);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(170, 22);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(6, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Identification";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(6, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Serveur";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Addresse du serveur :";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(182, 127);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(170, 22);
+            this.txtHost.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Port :";
+            // 
+            // btnTestConnection
+            // 
+            this.btnTestConnection.Location = new System.Drawing.Point(87, 183);
+            this.btnTestConnection.Name = "btnTestConnection";
+            this.btnTestConnection.Size = new System.Drawing.Size(170, 23);
+            this.btnTestConnection.TabIndex = 4;
+            this.btnTestConnection.Text = "Tester la connexion";
+            this.btnTestConnection.UseVisualStyleBackColor = true;
+            this.btnTestConnection.Click += new System.EventHandler(this.btnTestConnection_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 244);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Dossier de stockage :";
+            // 
+            // folderBrowserDialog
+            // 
+            //this.folderBrowserDialog.RootFolder = System.Environment.SpecialFolder.MyDocuments;
+            // 
+            // txtExportFolder
+            // 
+            this.txtExportFolder.Location = new System.Drawing.Point(182, 241);
+            this.txtExportFolder.Name = "txtExportFolder";
+            this.txtExportFolder.Size = new System.Drawing.Size(170, 22);
+            this.txtExportFolder.TabIndex = 5;
+            // 
+            // btnExportFolder
+            // 
+            this.btnExportFolder.Location = new System.Drawing.Point(358, 239);
+            this.btnExportFolder.Name = "btnExportFolder";
+            this.btnExportFolder.Size = new System.Drawing.Size(30, 23);
+            this.btnExportFolder.TabIndex = 6;
+            this.btnExportFolder.Text = "...";
+            this.btnExportFolder.UseVisualStyleBackColor = true;
+            this.btnExportFolder.Click += new System.EventHandler(this.btnExportFolder_Click);
+            // 
+            // nudPort
+            // 
+            this.nudPort.Location = new System.Drawing.Point(182, 156);
+            this.nudPort.Maximum = new decimal(new int[] {
+            65536,
+            0,
+            0,
+            0});
+            this.nudPort.Name = "nudPort";
+            this.nudPort.Size = new System.Drawing.Size(75, 22);
+            this.nudPort.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(6, 216);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Exportation";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(87, 269);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(170, 23);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Exporter";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 345);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(403, 23);
+            this.progressBar.TabIndex = 7;
+            this.progressBar.Visible = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 329);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 0;
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.label3);
+            this.groupBox.Controls.Add(this.label1);
+            this.groupBox.Controls.Add(this.nudPort);
+            this.groupBox.Controls.Add(this.label5);
+            this.groupBox.Controls.Add(this.btnExportFolder);
+            this.groupBox.Controls.Add(this.txtEmail);
+            this.groupBox.Controls.Add(this.btnExport);
+            this.groupBox.Controls.Add(this.label6);
+            this.groupBox.Controls.Add(this.btnTestConnection);
+            this.groupBox.Controls.Add(this.label7);
+            this.groupBox.Controls.Add(this.label8);
+            this.groupBox.Controls.Add(this.txtHost);
+            this.groupBox.Controls.Add(this.label4);
+            this.groupBox.Controls.Add(this.txtExportFolder);
+            this.groupBox.Controls.Add(this.txtPassword);
+            this.groupBox.Controls.Add(this.label2);
+            this.groupBox.Location = new System.Drawing.Point(12, 12);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(403, 304);
+            this.groupBox.TabIndex = 8;
+            this.groupBox.TabStop = false;
+            // 
+            // ImapExporter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 378);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblStatus);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ImapExporter";
+            this.Text = "IMAP Attachement Extrator";
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).EndInit();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnTestConnection;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.TextBox txtExportFolder;
+        private System.Windows.Forms.Button btnExportFolder;
+        private System.Windows.Forms.NumericUpDown nudPort;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.GroupBox groupBox;
+
+    }
+}
+
